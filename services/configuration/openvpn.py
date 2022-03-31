595391fe8aa1305d451571746c7dcf4503afe7c0
stepan <sshishigin190101@gmail.com>
2022-03-31 19:55:24 +0300

    fix

--- services/configuration/openvpn.py
+++ services/configuration/openvpn.py
@@ -2,6 +2,7 @@ from copy import copy
 
 from services.configuration.general import Configurator
 from services.helpers import EasyRSA, Container, get_container_id
+from settings import HOST_IP, PRIMARY_DNS, SECONDARY_DNS
 
 template = \
 """
@@ -12,7 +13,6 @@ resolv-retry infinite
 nobind
 persist-key
 persist-tun
-$OPENVPN_NCP_DISABLE
 cipher $OPENVPN_CIPHER
 auth $OPENVPN_HASH
 verb 3
@@ -51,18 +51,28 @@ class OpenVPNClientConfigurator(Configurator):
     def generate_configuration(self, client_name, platform="linux"):
         rsa = EasyRSA(self.container)
         data = rsa.create_new_client(client_name)
-        data.update({"platform": platform})
+        data.update({"platform": platform, "host": HOST_IP})
+        data.update(
+            OpenVPNServerConfigurator().get_server_settings()
+        )
         config = self.__build_config(data)
         return config
 
     def __build_config(self, data):
         config = template
-        OpenVPNServerConfigurator().get_server_settings()
-
         config = config.replace("$OPENVPN_CA_CERT", data.get("CA"))
         config = config.replace("$OPENVPN_CLIENT_CERT", data.get("client_certificate"))
         config = config.replace("$OPENVPN_PRIV_KEY", data.get("pk"))
         config = config.replace("$OPENVPN_TA_KEY", data.get("ta"))
+        config = config.replace("$REMOTE_HOST", data.get("host"))
+        config = config.replace("$OPENVPN_PORT", data.get("port"))
+        config = config.replace("$OPENVPN_CIPHER", data.get("cipher"))
+        config = config.replace("$PRIMARY_DNS", PRIMARY_DNS)
+        config = config.replace("$SECONDARY_DNS", SECONDARY_DNS)
+        config = config.replace("$OPENVPN_TRANSPORT_PROTO", data.get("proto"))
+        config = config.replace("$OPENVPN_HASH", data.get("auth"))
+
+
         if data.get("platform") != "windows":
             config = config.replace("block-outside-dns", "")
         print(config)
@@ -76,6 +86,14 @@ class OpenVPNServerConfigurator(Configurator):
 
     def get_server_settings(self):
         server_config = self.container.get_file_content("/opt/amnezia/openvpn/server.conf")
-        print(server_config, type(server_config))
+        server_config = server_config.split("\n")
+        data_collected = {}
+        for config_line in server_config:
+            parameter, value = config_line.split(" ")[0], config_line.split(" ")[1:]
+            value = " ".join(value)
+            data_collected.update({parameter: value})
+        return data_collected
+
+
 
 
