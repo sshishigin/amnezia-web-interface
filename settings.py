602f2da786f07a1e5358780de54f36cb6c0eff0b
stepan <sshishigin190101@gmail.com>
2022-03-31 17:07:18 +0300

    auto container ID

--- settings.py
+++ settings.py
@@ -1,5 +1,7 @@
+import subprocess
+
 PKI_PATH = "/opt/amnezia/openvpn"
-OPENVPN_CONTAINER_ID = ""
+OPENVPN_CONTAINER_ID = subprocess.check_output("docker ps | grep openvpn".split(" "))[0]
 CA_CERT_PATH = "/opt/amnezia/openvpn/ca.crt"
 CLIENT_CERT_FOLDER = "/opt/amnezia/openvpn/pki/issued"
 PRIVATE_KEY_FOLDER = "/opt/amnezia/openvpn/pki/private"
