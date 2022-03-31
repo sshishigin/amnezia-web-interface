38ad13c93e96cfeb76a44dc4f7f503db587dd433
stepan <sshishigin190101@gmail.com>
2022-03-31 20:40:47 +0300

    fix

--- services/helpers.py
+++ services/helpers.py
@@ -20,7 +20,7 @@ class Container:
         subprocess.call(docker_exec)
 
     def exec_easyrsa(self, client_name):
-        cmd_2 = f"easyrsa --passin=file:dh.pem --passout=file:dh.pem  build-client-full {client_name}"
+        cmd_2 = f"easyrsa --passin=file:dh.pem --passout=file:dh.pem  build-client-full {client_name} nopass"
         cmd_1 = f"docker exec -it {self.id} cd /opt/amnezia/openvpn | {cmd_2}".split(" ")
         subprocess.call(cmd_1, stdout=subprocess.PIPE)
 
